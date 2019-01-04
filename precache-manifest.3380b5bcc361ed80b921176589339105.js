69c8d04fdca08a8b3c008e29ee5d4308dabcfcf5
Mohamedox <ghemid_m_dz@yahoo.fr>
2019-01-04 17:16:08 +0100

    Updates

--- precache-manifest.3380b5bcc361ed80b921176589339105.js
+++ precache-manifest.3380b5bcc361ed80b921176589339105.js
@@ -4,7 +4,7 @@ self.__precacheManifest = [
     "url": "/hakima-qcm/static/media/exam-correct-answers.f6a6305a.png"
   },
   {
-    "revision": "1f8b0682b1adc53ec3b6",
+    "revision": "cfb762e4a13216c903f5",
     "url": "/hakima-qcm/static/css/main.4a781acf.chunk.css"
   },
   {
@@ -36,8 +36,8 @@ self.__precacheManifest = [
     "url": "/hakima-qcm/static/media/exam-date.a5307ef7.jpg"
   },
   {
-    "revision": "1f8b0682b1adc53ec3b6",
-    "url": "/hakima-qcm/static/js/main.1f8b0682.chunk.js"
+    "revision": "cfb762e4a13216c903f5",
+    "url": "/hakima-qcm/static/js/main.cfb762e4.chunk.js"
   },
   {
     "revision": "a9fbf540f602a533daf4eaa9d0c86f5c",
@@ -72,7 +72,7 @@ self.__precacheManifest = [
     "url": "/hakima-qcm/static/css/1.307a77a4.chunk.css"
   },
   {
-    "revision": "2c1db5cb6498d4438c14aa4eb385044f",
+    "revision": "32520f37834ed4c59d30a4b25e9fb67c",
     "url": "/hakima-qcm/index.html"
   }
 ];
